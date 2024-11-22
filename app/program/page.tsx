f9d59e8a6eee48d2c8513c3d5baa10bfa6f7b324
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2024-11-22 11:14:15 -0500

    hotfix for pdf view

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -1,9 +1,13 @@
-import PDFView from "@/components/PDFView/PDFView";
+"use client";
 
+import PDFView from "@/components/PDFView/PDFView";
+import { useMediaQuery } from "@mantine/hooks";
 export default function Page() {
+    const isMobile = useMediaQuery("(max-width: 768px)");
+    const filePath = isMobile ? "/fall-2024-program-small.pdf" : "/fall-2024-program.pdf";
     return (
         <>
-            <PDFView filePath="/fall-2024-program.pdf" />
+            <PDFView filePath={filePath} />
         </>
     );
 }
