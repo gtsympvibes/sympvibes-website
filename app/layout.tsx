03727ed3d8e9db83ab1dc1439e0e920b15ee1ad5
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2025-01-03 21:01:21 -0500

    update favicon images

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,6 @@ export default function RootLayout({ children }: { children: any }) {
         <html lang="en">
             <head>
                 <ColorSchemeScript forceColorScheme="light" />
-                <link rel="shortcut icon" href="/favicon.svg" />
                 <meta
                     name="viewport"
                     content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
