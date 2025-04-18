a27295e68f9e1fbe2570f8601df27b3c063507b2
Reetesh Sudhakar <rsudhakar9@gatech.edu>
2025-04-18 12:13:28 -0400

    spring 25 programs

--- app/program/page.tsx
+++ app/program/page.tsx
@@ -4,7 +4,7 @@ import PDFView from "@/components/PDFView/PDFView";
 import { useMediaQuery } from "@mantine/hooks";
 export default function Page() {
     const isMobile = useMediaQuery("(max-width: 768px)");
-    const filePath = isMobile ? "/fall-2024-program-small.pdf" : "/fall-2024-program.pdf";
+    const filePath = isMobile ? "/program-small.pdf" : "/program.pdf";
     return (
         <>
             <PDFView filePath={filePath} />
